f32e23855d59984e1a88a6ba9097302e3ab89ebc
felipe11 <felipe11@astro.ufrj.br>
2018-09-27 14:27:03 -0300

    ...

---
+++ js/load_layout.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+  
+  $('#header').load("/layout/header.txt");
+  $('#navbar').load("/layout/navbar.txt");
+  $('#sidebar_social').load("/layout/sidebar_social.txt");
+  $('#sidebar_feature').load("/layout/sidebar_feature.txt");
+  $('#sidebar_playlists').load("/layout/sidebar_playlists.txt");
+  $('#sidebar_partners').load("/layout/sidebar_partners.txt");
+  $('#slider').load("/layout/slider.txt");
+  $('#videos').load("/layout/videos.txt");
+  $('#noticias').load("/layout/noticias.txt");
+  $('#playlists').load("/layout/playlists.txt");
+  $('#recommended').load("/layout/recommended.txt");
+  $('#personnel').load("/layout/personnel.txt");
+  $('#contact').load("/layout/contact.txt");
+  $('#footer').load("/layout/footer.txt");
+});
+
